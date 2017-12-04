9168ac29e46d99ab161775ed82eab0f1a53ab9b7
anugnes <nugnes.alberto@gmail.com>
2017-12-04 16:57:07 +0000

    Add Ansible playbook to configure instance

---
+++ main.tf
@@ -0,0 +1,98 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["paravirtual"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_security_group" "openvpn" {
+  name        = "${var.name}"
+  vpc_id      = "${var.vpc_id}"
+  description = "OpenVPN security group"
+
+  tags {
+    Name = "${var.name}"
+  }
+
+  ingress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  # For OpenVPN Client Web Server & Admin Web UI
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "udp"
+    from_port   = 1194
+    to_port     = 1194
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = -1
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ebs_volume" "openvpn_data" {
+  availability_zone = "${var.ebs_region}"
+  size              = "${var.ebs_size}"
+  encrypted         = true
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_instance" "openvpn" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${var.key_name}"
+  subnet_id     = "${element(split(",", var.public_subnet_ids), count.index)}"
+
+  vpc_security_group_ids = ["${aws_security_group.openvpn.id}"]
+
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  instance_id = "${aws_instance.openvpn.id}"
+  volume_id   = "${aws_ebs_volume.openvpn_data.id}"
+
+  provisioner "local-exec" {
+    command = "ansible-playbook --private-key ~/.ssh/id_rsa.pub -i '${aws_instance.openvpn.public_ip},' ~/ansible/openvpn.yml"
+  }
+}
+
+resource "aws_route53_record" "openvpn" {
+  zone_id = "${var.route_zone_id}"
+  name    = "vpn.${var.domain}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${aws_instance.openvpn.public_ip}"]
+}
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  default     = "openvpn"
+  description = "OpenVPN instance name"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC to use"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDRs to use"
+}
+
+variable "public_subnet_ids" {
+  description = "Public Subnet IDs"
+}
+
+variable "route_zone_id" {
+  description = "Route Zone ID"
+}
+
+variable "domain" {
+  description = "Public domain to assign to the openVPN host. With a value of 'example.com' The resulting value will be 'vpn.example.com"
+}
+
+variable "instance_type" {
+  description = "OPenVPN EC2 instance type"
+}
+
+variable "key_name" {
+  description = "Key Pair name"
+}
+
+variable "ebs_region" {
+  description = "Region for the EBS volume where we'll store credentials and certificates"
+}
+
+variable "ebs_size" {
+  description = "EBS volume size. 1GB should be fine in most cases"
+}
